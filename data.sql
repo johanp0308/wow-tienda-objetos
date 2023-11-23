0a63d72c644a1c107165693132a180864faf23c7
Johan Pachon <sebastian-johan1@hotmail.com>
2023-11-23 07:28:27 -0500

    feat: :sparkles: Update 'data.sql' added constraint

--- data.sql
+++ data.sql
@@ -1,30 +1,46 @@
 CREATE TABLE object(
-    id_object VARCHAR(255) NOT NULL,
-    name_object VARCHAR(255) NOT NULL,
-    type_object VARCHAR(255) NOT NULL,
-    category ENUM("comun",) NOT NULL
+    id_object VARCHAR(50) NOT NULL,
+    name_object VARCHAR(50) NOT NULL,
+    type_object VARCHAR(40) NOT NULL,
+    category ENUM('pobre','comun','poco comun','raro'.'epico','legendario','artefacto') NOT NULL
 );
+
+ALTER TABLE object 
+ADD CONSTRAINT UQ_object_name UNIQUE(name_object)
+ADD CONSTRAINT PK_object PRIMARY KEY (id_object);
 
 CREATE TABLE faction(
     id INT NOT NULL AUTO_INCREMENT,
-    faction VARCHAR(255) NOT NULL
+    faction VARCHAR(20) NOT NULL
 );
 
+ALTER TABLE faction
+ADD CONSTRAINT UQ_faction_faction UNIQUE(faction)
+ADD CONSTRAINT PK_faction PRIMARY KEY (id);
+
 CREATE TABLE buy(
-    id_compra VARCHAR(255) NOT NULL,
-    user_name VARCHAR(255) NOT NULL,
-    id_object VARCHAR(255) NOT NULL
+    id_buy VARCHAR(30) NOT NULL,
+    user_name VARCHAR(20) NOT NULL,
+    id_object VARCHAR(50) NOT NULL
 );
+
+ALTER TABLE buy ADD CONSTRAINT PK_buy PRIMARY KEY (id_buy);
 
 CREATE TABLE type_object(
     id INT NOT NULL AUTO_INCREMENT,
     type VARCHAR(255) NOT NULL
 );
+
+ALTER TABLE type_object ADD CONSTRAINT PK_type_object PRIMARY KEY (id);
+
 CREATE TABLE stats_object(
+    id_stats_object INT NOT NULL,
     id_object VARCHAR(255) NOT NULL,
     id_stats INT NOT NULL,
     value DOUBLE(8, 2) NOT NULL
 );
+
+ALTER TABLE stats_object ADD CONSTRAINT PK_stats_object PRIMARY KEY (id_)
 
 CREATE TABLE inventory(
     id_inventory INT NOT NULL
